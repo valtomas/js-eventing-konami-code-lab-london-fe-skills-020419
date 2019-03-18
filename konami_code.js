2dde056c8113125490e96bbb656951ec19303efc
Tomas Val <tomas.val@wework.com>
2019-03-18 19:53:13 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,10 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 let konamiLenght = 0;
 
-function init() {
+// function init() {
 
 document.addEventListener('keydown', konamiCodeListener(e)); {
  
-  
 }
-}
+
