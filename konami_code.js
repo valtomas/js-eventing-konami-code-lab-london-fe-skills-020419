f538bf54850e01c7f6c94fa15937d1916a11ec57
Tomas Val <tomas.val@wework.com>
2019-03-18 20:11:31 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,7 @@ document.addEventListener('keydown', init(e));
 
 function init(e) {
   
-  let keypressed = e.keyCode ;
+  let keypressed = e.keyCode;
   
 if (code(konamiLenght) == keypressed ){
   
